154fe88b62ddc974c4b8144036c5ba1641730ca1
José David <josdagaro@gmail.com>
2018-07-29 03:31:19 -0500

    Log out functionality was added (simulation)

--- BanlineaTest/ClientApp/app/components/navmenu/navmenu.component.ts
+++ BanlineaTest/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { SessionService } from '../session/session.service';
+import { User } from '../interfaces/user';
 
 @Component({
     selector: 'nav-menu',
@@ -7,5 +9,11 @@ import { SessionService } from '../session/session.service';
     styleUrls: ['./navmenu.component.css']
 })
 export class NavMenuComponent {
-    constructor(session: SessionService) { }
+    constructor(private session: SessionService) { }
+
+    public logOut() {
+        if (this.session.user !== undefined && this.session.user.email !== undefined && this.session.user.name !== undefined) {
+            window.location.replace(window.location.pathname + window.location.search + window.location.hash);
+        }
+    }
 }
